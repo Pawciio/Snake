dcb4d6cb73c2b16a122dd49c6c04b90471cb8581
Pawciio <choldapawel@gmail.com>
2020-08-31 22:40:13 +0200

    event listener to move snake and draw function

--- app.js
+++ app.js
@@ -30,3 +30,31 @@ let food = {
 // controle the snake
 let d;
 
+// event listener to controle snake WSAD or ARROWS
+
+document.addEventListener("keydown", (e) => {
+    let key = e.keyCode;
+    if (key == 37 || key == 65 &&  d != "RIGHT") {
+        d = "LEFT";
+    }else if(key == 38 || key == 87 && d != "DOWN") {
+        d = "UP";
+    }else if(key == 39 || key == 68 && d != "LEFT") {
+        d = "RIGHT";
+    }else if(key == 40 || key == 83 && d != "UP") {
+        d = "DOWN";
+    }
+});
+
+// Draw snake and food in canvas
+
+function draw() {
+    for (let i = 0; i < snake.length; i++) {
+        ctx.fillStyle = (1 == 0 ) ? "green" : "white";
+        ctx.fillRect(snake[i].x, snake[i].y, box, box);
+
+        ctx.strokeStyle = "red";
+        ctx.strokeRect(snake[i].x, snake[i].y, box, box);
+    }
+    ctx.fillStyle = "red";
+    ctx.fillRect(food.x, food.y, box, box);
+};
